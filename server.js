1a64d22a0513c4ec3015a4d7d1bf7ac8e83927c7
Glitch (web-socket-example) <none>
2018-03-20 18:49:52 +0000

    💰🍱 Checkpoint ./package.json:80226/1149 ./server.js:80226/2289 ./views/index.html:80226/2161 ./public/monitor.html:80226/692 ./public/monitor.js:80226/649 ./public/trigger.html:80226/698

--- server.js
+++ server.js
@@ -1,39 +1,32 @@
-// server.js
-// where your node app starts
+//a very very simple server
 
-// init project
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+var io = require('socket.io')(server);    //http://socket.io/docs/
+var name_spaced_com = io.of('/dimas_channel'); //change this name!!!
+
+name_spaced_com.on('connection', function (socket) {
+  console.log("Client ID"+socket.id+" connected");
+
+  app.get('/trigger_1', function(request,response){
+    // response.json({"status":"success"});
+    response.sendStatus(200);
+    console.log("trigger 1 pressed");
+    name_spaced_com.emit('trigger_1');
+  })
+
+  app.get('/trigger_2', function(request,response){
+    // response.json({"status":"success"});
+    response.sendStatus(200);
+    console.log("trigger 2 pressed");
+    name_spaced_com.emit('trigger_2');
+  })
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
+var server = app.listen(process.env.PORT || 8081, function () {
+  console.log("Shazam! listening on port 8081");
 })
