360430545e8d67f8474720fcb9cf881b3875e1d7
Glitch (bone-lizard) <none>
2018-10-16 08:58:50 +0000

    🏄🏦 Checkpoint ./public/index.js:244840/35

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const mimeType = 'video/webm; codecs=vp9';
 // Record screen video stream and broadcast stream to server
 navigator.getDisplayMedia({ video: true }).then(stream => {
   const mediaRecorder = new MediaRecorder(stream, { mimeType });
-  mediaRecorder.start(1000);
+  mediaRecorder.start(1000 /* TODO */);
   mediaRecorder.addEventListener('dataavailable', function(e) {
     socket.emit('broadcast', { blob: e.data });
   });
