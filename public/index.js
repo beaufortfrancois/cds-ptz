632e611b8b70add0e32b71fb8175389c7b08219a
Glitch (bone-lizard) <none>
2018-10-16 11:10:49 +0000

    🌽🔦 Checkpoint ./public/index.js:244840/912 ./public/index.html:244840/294

--- public/index.js
+++ public/index.js
@@ -4,30 +4,32 @@ const mimeType = 'video/webm; codecs=vp9';
 const pipVideo = document.createElement('video');
 pipVideo.autoplay = true;
 pipVideo.muted = true;
-/* BUG */ pipVideo.style.width = 1; document.body.appendChild(pipVideo);
+/* BUG */ pipVideo.style.width = '1px'; document.body.appendChild(pipVideo);
 
 navigator.mediaDevices.getUserMedia({ video: true }).then(cameraVideoStream => {
   pipVideo.srcObject = cameraVideoStream;
 });
 
+video.onclick = _ => { pipVideo.requestPictureInPicture(); };
 
-video.addEventListener('click', function() {
+pipVideo.onenterpictureinpicture = async _ => {
 
-pipVideo.requestPictureInPicture();
+  const screenVideoStream = await navigator.getDisplayMedia({ video: true });
+  const voiceAudioStream  = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-// Record screen video stream and broadcast stream to server
-navigator.getDisplayMedia({ video: true }).then(stream => {
+  const stream = new MediaStream([
+    ...screenVideoStream.getTracks(),
+    ...voiceAudioStream.getTracks()
+  ]);
+
+  // Record screen video stream and broadcast stream to server
   const mediaRecorder = new MediaRecorder(stream, { mimeType });
-
   mediaRecorder.start();
   mediaRecorder.ondataavailable = event => {
     socket.emit('broadcast', { blob: event.data });
   }
   setInterval(_ => mediaRecorder.requestData(), 1000);
-});
-
-  
-});
+}
 
 // Receive video stream from server and play it back.
 const mediaSource = new MediaSource();
