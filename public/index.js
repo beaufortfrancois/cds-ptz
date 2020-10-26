cc6d6954f99543e9810a15bfa2e3a3284d216dc6
Glitch (bone-lizard) <none>
2020-10-26 15:15:54 +0000

    🏭👾 Checkpoint ./public/index.js:244840/3088

--- public/index.js
+++ public/index.js
@@ -1,20 +1,16 @@
 const socket = io();
-const mimeType = "video/webm; codecs=vp9,opus";
+const mimeType = "video/webm; codecs=vp9";
 
 /* Recording */
 
+let stream;
+
 async function getUserMedia() {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  stream = await navigator.mediaDevices.getUserMedia({
     video: { pan: true, tilt: true, zoom: true }
   });
-
-  const video = document.createElement("video");
-  video.autoplay = true;
-  video.muted = true;
-  video.srcObject = stream;
-  video.play();
-
-  document.body.classList.add("broadcasting");
+  
+  // video.srcObject = stream;
 
   // Record screen video stream and broadcast stream to server
   const mediaRecorder = new MediaRecorder(stream, { mimeType });
@@ -25,38 +21,32 @@ async function getUserMedia() {
   };
 }
 
-/* Playback video */
+// /* Playback video */
 
 let chunks = [];
 
 // Receive video stream from server and play it back.
 const mediaSource = new MediaSource();
 video.src = URL.createObjectURL(mediaSource);
-mediaSource.onsourceopen = _ => {
-  const sourceBuffer = mediaSource.addSourceBuffer(mimeType);
+mediaSource.addEventListener(
+  "sourceopen",
+  () => {
+    const sourceBuffer = mediaSource.addSourceBuffer(mimeType);
+    console.log(sourceBuffer);
 
-  socket.on("playback", event => {
-    chunks.push(event.blob);
-    appendBuffer();
+    socket.on("playback", event => {
+      chunks.push(event.blob);
+      // appendBuffer();
 
-    // Add controls to unmute video.
-    if (!document.pictureInPictureElement && !video.controls) {
-      video.addEventListener(
-        "playing",
-        _ => {
-          video.controls = true;
-        },
-        { once: true }
-      );
-    }
-    document.body.classList.add("playing");
-    video.removeEventListener("click", onVideoFirstClick);
-  });
+      document.body.classList.add("playing");
+    });
 
-  function appendBuffer() {
-    if (sourceBuffer.updating || chunks.length === 0) return;
+//     function appendBuffer() {
+//       if (sourceBuffer.updating || chunks.length === 0) return;
 
-    sourceBuffer.appendBuffer(chunks.shift());
-    sourceBuffer.addEventListener("updateend", appendBuffer, { once: true });
-  }
-};
+//       sourceBuffer.appendBuffer(chunks.shift());
+//       sourceBuffer.addEventListener("updateend", appendBuffer, { once: true });
+//     }
+  },
+  { once: true }
+);
