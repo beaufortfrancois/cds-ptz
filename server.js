7aa16829a3b06c1d19066d5cfec2169a8784d1ea
Glitch (bone-lizard) <none>
2020-11-12 13:04:16 +0000

    🕵🏨 Checkpoint ./server.js:244840/2896 ./package.json:244840/2

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ io.on("connection", socket => {
 
   socket.on("broadcast", data => {
     if (videoStreamingSocket != socket) {
+      // Store snapshot of first bytes.
       lastFirstData = data;
       socket.emit("lastFirstData", lastFirstData);
     }
@@ -26,18 +27,22 @@ io.on("connection", socket => {
     // Broadcast video stream to all connected clients.
     io.emit("playback", data);
   });
+
   socket.on("camera", data => {
     // Send new camera values to the video streaming socket client.
     videoStreamingSocket.emit("camera", data);
   });
+
   socket.on("settings", data => {
     // Broadcast ptz settings to all connected clients.
     io.emit("settings", data);
   });
+
   socket.on("capabilities", data => {
     // Broadcast ptz capabilities to all connected clients.
     io.emit("capabilities", data);
   });
+
   socket.on("disconnect", reason => {
     numberOfClientsConnected--;
     io.emit("clients", {
